3d1fa256ee786ea97acfbcf23e77841c9031bf29
AlexNilson-hub <alexnilson01@gmail.com>
2022-01-17 21:59:15 +0300

    commit-finish-app

--- src/components/info.js
+++ src/components/info.js
@@ -1,15 +1,37 @@
 import React from "react";
 
-class Info extends React.Component {
-    render() {
-        return(
-            <div>
-                <h2>Погодное приложение</h2>
-                <p>Узнайте погоду в вашем городе!</p>
-
-            </div>
-        )
-    }
-}
+//Упрощенный/оптимизированный код
+const Info = () => (
+    <div>
+        <h2>Погодное приложение</h2>
+        <p>Узнайте погоду в вашем городе!</p>
+    </div>
+);
 
 export default Info;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Info extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <h2>Погодное приложение</h2>
+//                 <p>Узнайте погоду в вашем городе!</p>
+//
+//             </div>
+//         )
+//     }
+// }
